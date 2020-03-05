705247363d8a02a608b7dfa7ec0d7a12264e6f71
LainaJ <lainakarosic@gmail.com>
2020-03-04 20:56:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+
+  componentDidMount(){
+    fetch('https://sandbox-api.dexcom.com')
+    .then(resp => resp.json())
+    .then(dexcomData => console.log(dexcomData))
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+    }
+
+  }
 
 export default App;
