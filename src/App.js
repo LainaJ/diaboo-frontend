83cde3c60342c2b96287e1fee7c91d33465f0bd6
LainaJ <lainakarosic@gmail.com>
2020-03-22 14:12:39 -0400

    trial primes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-class App extends React.Component {
+// class App extends React.Component {
 
+  // https://sandbox-api.dexcom.com/v2/oauth2/login?client_id={your_client_id}&redirect_uri={your_redirect_uri}&response_type=code&scope=offline_access&state={your_state_value}
+  // https://sandbox-api.dexcom.com
+  // https://sandbox-api.dexcom.com/v2/oauth2/login?client_id=8nX2snn5pfz6shrvTZ2QEEow0FS12Kpf&redirect_uri={your_redirect_uri}&response_type=code&scope=offline_access
 
-  componentDidMount(){
-    fetch('https://sandbox-api.dexcom.com')
-    .then(resp => resp.json())
-    .then(dexcomData => console.log(dexcomData))
-  }
+  // componentDidMount(){
+  // "Access-Control-Allow-Origin"
+  //   fetch('https://sandbox-api.dexcom.com')
+  //   .then(resp => resp.json())
+  //   .then(dexcomData => console.log(dexcomData))
+  // }
 
-  render() {
-    return (
-      <div>
+  console.time("concatenation")
 
-      </div>
-    )
+  function isPrime(num) {
+    //if number is 0 or 1, not prime
+    if (num < 2){
+      return false
     }
-
+    //loop through, test dividing all numbers between 2-num(not including num); eliminate even nums
+    for (let i = 2; i < num; i++){
+      if (num % i === 0) {
+        return false
+      }
+    }
+    return true 
   }
 
-export default App;
+// 10 => 2, 3, 5, 7
+  function allPrime(num) {
+    let primes = []
+    for (let i = 2; i < num; i++){
+      if (isPrime(i)) {
+        primes.push(i)
+      }
+    }
+    return primes 
+  }
+
+  console.log(allPrime(10))
+  console.timeEnd("concatenation end")
+ 
+
+  // render() {
+  //   return (
+  //     <div>
+  //       Hey there!
+  
+  //     {console.log("allPrime", this.isPrime(10))}
+  //     {console.log("allPrime2", this.allPrime(10))}
+
+      
+  //     </div>
+  //   )
+  //   }
+
+  // }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+console.time("concatenation")
+
+function isPrime(num) {
+  if (num < 2){
+    return false
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++){
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true 
+}
+
+function allPrime(num) {
+  let primes = []
+  for (let i = 2; i < num; i++){
+    if (isPrime(i)) {
+      primes.push(i)
+    }
+  }
+  return primes 
+}
+
+
+console.log(allPrime(10))
+console.timeEnd("concatenation")
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
